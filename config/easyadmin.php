766304ca7a91cc657978336b93f8f4fcbe8d0c4b
xiangbing <nails-totems0k@icloud.com>
2024-07-18 17:54:55 +0800

    init

--- config/easyadmin.php
+++ config/easyadmin.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'ADMIN'             => env('EASYADMIN.ADMIN', 'admin'),
-    'CAPTCHA'           => env('EASYADMIN.CAPTCHA', false),
-    'IS_DEMO'           => env('EASYADMIN.IS_DEMO', false),
-    'CDN'               => env('EASYADMIN.CDN', ''),
-    'EXAMPLE'           => env('EASYADMIN.EXAMPLE', true),
-    'IS_CSRF'           => env('EASYADMIN.IS_CSRF', false),
-    'STATIC_PATH'       => env('EASYADMIN.STATIC_PATH', '/static'),
-    'OSS_STATIC_PREFIX' => env('EASYADMIN.OSS_STATIC_PREFIX', 'static_easyadmin'),
+    'ADMIN'             => env('EASYADMIN_ADMIN', 'admin'),
+    'CAPTCHA'           => env('EASYADMIN_CAPTCHA', false),
+    'IS_DEMO'           => env('EASYADMIN_IS_DEMO', false),
+    'CDN'               => env('EASYADMIN_CDN', ''),
+    'EXAMPLE'           => env('EASYADMIN_EXAMPLE', true),
+    'IS_CSRF'           => env('EASYADMIN_IS_CSRF', false),
+    'STATIC_PATH'       => env('EASYADMIN_STATIC_PATH', '/static'),
+    'OSS_STATIC_PREFIX' => env('EASYADMIN_OSS_STATIC_PREFIX', 'static_easyadmin'),
 ];
